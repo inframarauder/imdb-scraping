44f816a7e9f7ba916be9e92037386d9d92b1efff
subhasis020299 <subhasisdas125@gmail.com>
2020-07-21 20:55:16 +0530

    scraping complete

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ movie_links = scrape.get_movie_links()
 movie_data_list = []
 
 print('Scraping movie info:')
+count = 0
 for link in movie_links:
+    count += 1
     data = scrape.get_movie_data(link)
     movie_data_list.append(data)
-    print(data['name'])
+    print(str(count)+'.'+data['name'])
 
 print(json.dumps(movie_data_list))
