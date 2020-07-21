5c7d7aa374d5d3a64dbd9288ec62c15841b4a678
subhasis020299 <subhasisdas125@gmail.com>
2020-07-21 21:42:14 +0530

    added dotenv

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+# scrape top X movies from IMDB and write the data to MongoDB.
+# X can be atmost 250 (as per IMDB)
+
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import scrape
+import os
+
+# load env variables:
+load_dotenv()
 
 # create mongodb connection:
-client = MongoClient(
-    'mongodb+srv://dbadmin:I7hWIWrLDrq18JqZ@pmdb-cluster.txef4.mongodb.net/')
+db_uri = os.getenv('MONGO_CONN_STRING')
+client = MongoClient(db_uri)
 db = client['pmdb']
 
 if len(list(db.movies.find())) > 0:
@@ -13,11 +21,12 @@ movie_links = scrape.get_movie_links()
 
 
 print('Scraping movie info:')
+X = 10
 count = 0
 for link in movie_links:
-    if count > 2:
-        break
     count += 1
+    if count > X:
+        break
     data = scrape.get_movie_data(link)
     db.movies.insert_one(data)
-    print(str(count)+'.'+data['name'])
+    print(str(count)+'.'+data['name'] + ' written to DB')
