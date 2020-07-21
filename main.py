fc8c775a0c502c909d658e75456dee5a896e917a
subhasis020299 <subhasisdas125@gmail.com>
2020-07-21 21:25:45 +0530

    pymongo integration done

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+from pymongo import MongoClient
 import scrape
-import json
+
+# create mongodb connection:
+client = MongoClient(
+    'mongodb+srv://dbadmin:I7hWIWrLDrq18JqZ@pmdb-cluster.txef4.mongodb.net/')
+db = client['pmdb']
+
+if len(list(db.movies.find())) > 0:
+    db.movies.delete_many({})
 
 movie_links = scrape.get_movie_links()
-movie_data_list = []
+
 
 print('Scraping movie info:')
 count = 0
 for link in movie_links:
+    if count > 2:
+        break
     count += 1
     data = scrape.get_movie_data(link)
-    movie_data_list.append(data)
+    db.movies.insert_one(data)
     print(str(count)+'.'+data['name'])
-
-print(json.dumps(movie_data_list))
